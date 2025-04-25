7a036e29b89bc0b8e8c99a09646766530f09f8fa
Jayrr <samuel.v.soriano@gmail.com>
2025-04-25 13:30:03 -0600

    Update ContactPage to enhance map iframe dimensions and styling
    - Adjusted the width of the Google Maps iframe from 600px to 800px for better visibility.
    - Maintained the height at 450px and ensured consistent styling for the iframe container.

--- app/(main-pages)/contact/page.tsx
+++ app/(main-pages)/contact/page.tsx
@@ -349,8 +349,8 @@ export default function ContactPage() {
             </div>
           </div>
 
-          <div className="relative h-[300px] rounded-lg overflow-hidden">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4740.956036094691!2d-113.63212153915934!3d53.54923380401824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a020de2209832f%3A0xebd2b79d62c20f2d!2sEdmonton%2C%20AB%20T5S%202H1!5e0!3m2!1sen!2sca!4v1745604616646!5m2!1sen!2sca" width="600" height="450" style={{border:0}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <div className="relative h-[300px] rounded-lg overflow-hidden ">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4740.956036094691!2d-113.63212153915934!3d53.54923380401824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a020de2209832f%3A0xebd2b79d62c20f2d!2sEdmonton%2C%20AB%20T5S%202H1!5e0!3m2!1sen!2sca!4v1745604616646!5m2!1sen!2sca" width="800" height="450" style={{border:0}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center">
               <Button 
                 variant="outline" 
